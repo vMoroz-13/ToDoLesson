9d21b02fd82c73172b14e881ece73b0bcca7bfeb
Vadim Moroz <vmoroz13@yandex.ru>
2020-06-18 11:16:53 +0300

    Исправил 6-7

---
+++ script/oop.js
@@ -0,0 +1,30 @@
+
+alert('kkk')
+
+const start = document.getElementById('start');
+const btnPluses = document.getElementsByTagName('button');
+const btnPlusIncome = document.getElementsByTagName('button')[0];
+const btnPlusExpenses = document.getElementsByTagName('button')[1];
+const depositCheck = document.querySelector('#deposit-check');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const budgetMonthValue = document.querySelector('.budget_month-value');
+const budgetDayValue = document.querySelector('.budget_day-value');
+const expensesMonthValue = document.querySelector('.expenses_month-value');
+const additionalIncomeValue = document.querySelector('.additional_income-value');
+const additionalExpensesValue = document.querySelector('.additional_expenses-value');
+const incomePeriodValue = document.querySelector('.income_period-value');
+const targetMonthValue = document.querySelector('.target_month-value');
+const inputSalarAymount = document.querySelector('.salary-amount');
+const inputIncomeTitle = document.querySelector('.income-title');
+const inputIncomeAmount = document.querySelector('.income-amount');
+const inputExpensesTitle = document.querySelector('.expenses-title');
+const inputExpensesAmount = document.querySelector('.expenses-amount');
+const inputAdditionalIncomeItem = document.querySelector('.additional_income-item');
+const inputAdditionalIncomeAmount = document.querySelector('.additional_income-amount');
+const inputAdditionalExpensesItem = document.querySelector('.additional_expenses-item');
+const inputDepositCheck = document.querySelector('#deposit-check');
+const inputDepositAmount = document.querySelector('.deposit-amount');
+const inputDepositPercent = document.querySelector('.deposit-percent');
+const inputTargetAmount = document.querySelector('.target-amount');
+const inputPeriodSelect = document.querySelector('.period-select');
+
