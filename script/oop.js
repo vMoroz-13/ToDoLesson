b7e0c32a8beb6425c660d92310adfe94cd9aa31d
Vadim Moroz <vmoroz13@yandex.ru>
2020-06-19 09:51:57 +0300

    incom-item исправил

--- script/oop.js
+++ script/oop.js
@@ -1,10 +1,10 @@
 
-alert('kkk')
+'use strict';
 
 const start = document.getElementById('start');
 const btnPluses = document.getElementsByTagName('button');
-const btnPlusIncome = document.getElementsByTagName('button')[0];
-const btnPlusExpenses = document.getElementsByTagName('button')[1];
+const btnPlusIncome = btnPluses[0];
+const btnPlusExpenses = btnPluses[1];
 const depositCheck = document.querySelector('#deposit-check');
 const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
 const budgetMonthValue = document.querySelector('.budget_month-value');
@@ -15,11 +15,13 @@ const additionalExpensesValue = document.querySelector('.additional_expenses-val
 const incomePeriodValue = document.querySelector('.income_period-value');
 const targetMonthValue = document.querySelector('.target_month-value');
 const inputSalarAymount = document.querySelector('.salary-amount');
-const inputIncomeTitle = document.querySelector('.income-title');
+const inputIncomeTitle = document.querySelector('input.income-title');
 const inputIncomeAmount = document.querySelector('.income-amount');
 const inputExpensesTitle = document.querySelector('.expenses-title');
 const inputExpensesAmount = document.querySelector('.expenses-amount');
+
 const inputAdditionalIncomeItem = document.querySelector('.additional_income-item');
+
 const inputAdditionalIncomeAmount = document.querySelector('.additional_income-amount');
 const inputAdditionalExpensesItem = document.querySelector('.additional_expenses-item');
 const inputDepositCheck = document.querySelector('#deposit-check');
@@ -28,3 +30,7 @@ const inputDepositPercent = document.querySelector('.deposit-percent');
 const inputTargetAmount = document.querySelector('.target-amount');
 const inputPeriodSelect = document.querySelector('.period-select');
 
+
+
+
+ 
